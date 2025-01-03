2a3e50cd9c40da63c276a1d6696c8d6544898009
Johan Rosa <johan.rosaperez@gmail.com>
2025-01-02 21:56:57 -0400

    google_keep: trigger modal

--- google_keep/app.js
+++ google_keep/app.js
@@ -9,6 +9,10 @@ class APP {
         this.$formButtons = document.querySelector("#form-buttons");
         this.$placeholder = document.querySelector("#placeholder");
         this.$notes = document.querySelector("#notes");
+        this.$modal = document.querySelector(".modal");
+        this.$modalTitle = document.querySelector(".modal-title");
+        this.$modalText = document.querySelector(".modal-text");
+        this.$modalClose = document.querySelector(".modal-close-button");
 
         this.addEventListeners();
     }
@@ -18,24 +22,31 @@ class APP {
             this.handleFormClick(event);
         });
 
+        this.$modalClose.addEventListener("click", () => {
+            this.closeModal();
+        });
+
+        this.$notes.addEventListener("click", event => {
+            if (this.$notes.contains(event.target)) {
+                const selectedNote = this.selectNote(event);
+                const note = this.notes.filter(note => note.id == selectedNote.id);
+
+                this.$modalTitle.value = note[0].title;
+                this.$modalText.value = note[0].text;
+                this.openModal(event);
+            }
+        });
+
         this.$form.addEventListener("submit", event => {
             event.preventDefault();
-            const text = this.$noteText.value;
-            const title = this.$noteTitle.value;
-
-            const hasNote = title || text;
-            if (hasNote) {
-                this.addNote({ title, text });
-            }
-
-            this.clearForm();
-            this.closeForm();
+            this.saveNote();
         });
     }
 
     handleFormClick(event) {
         const isFormClicked = this.$form.contains(event.target);
         isFormClicked ? this.openForm() : this.closeForm();
+        isFormClicked ? null : this.saveNote();
     }
 
     openForm() {
@@ -69,23 +80,53 @@ class APP {
         this.displayNotes();
     }
 
+    saveNote() {
+        const text = this.$noteText.value;
+        const title = this.$noteTitle.value;
+
+        const hasNote = title || text;
+        if (hasNote) {
+            this.addNote({ title, text });
+        }
+
+        this.clearForm();
+        this.closeForm();
+    }
+
+    selectNote(event) {
+        const $selectedNote = event.target.closest('.note');
+        console.log($selectedNote.id);
+        return $selectedNote;
+     }
+
     displayNotes() {
         const hasNotes = this.notes.length > 0;
         this.$placeholder.style.display = hasNotes ? "none" : "flex";
-        
+
         this.$notes.innerHTML = this.notes.map(note => `
-            <div style="background: ${note.color};" class="note">
+            <div style="background: ${note.color};" class="note" id="${note.id}">
                 <div class="${note.title && 'note-title'}">${note.title}</div>
                 <div class="note-text">${note.text}</div>
                 <div class="toolbar-container">
                     <div class="toolbar">
-                        <img class="toolbar-color" src="https://icon.now.sh/palette">
-                        <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                        <img class="toolbar-color" src="./palette.svg">
+                        <img class="toolbar-delete" src="./delete.svg">
                     </div>
                 </div>
             </div>
         `).join("");
     }
+
+    openModal(event) {
+        if (event.target.closest(".note")) {
+            this.$modal.classList.add("open-modal");
+        }
+    }
+
+    closeModal() {
+        this.$modal.classList.remove("open-modal");
+    }
+
 }
 
 new APP()
